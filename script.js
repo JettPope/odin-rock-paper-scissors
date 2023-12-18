95a93c44ece4c9b39e5c3b159354b3bc1c0f64f5
LeadMeteor <jettepope@gmail.com>
2023-12-18 17:31:43 -0600

    Begin adding game logic
    Work on function for each round of the game, comparing userChoice to
    computerChoice according to the rules of rock paper scissors.

--- script.js
+++ script.js
@@ -4,4 +4,42 @@ function getComputerChoice() {
     return moves[Math.floor(Math.random() * 3)];
 }
 
-console.log(getComputerChoice());
+function game(playerSelection, computerSelection) {
+    switch (playerSelection.toLowerCase()) {
+        case "rock":
+            if (computerSelection === "Paper") {
+                return "You lose (paper beats rock)!";
+            }
+            else if (computerSelection === "Scissors") {
+                return "You win (rock beats scissors)!";
+            }
+            else {
+                return "Tie (You both chose rock)!";
+            }
+            break;
+
+        case "paper":
+            if (computerSelection === "Scissors") {
+                return "You lose (scissors beat paper)!";
+            }
+            else if (computerSelection === "Rock") {
+                return "You win (paper beats rock)!";
+            }
+            else {
+                return "Tie (You both chose paper)!";
+            }
+            break;
+
+        case "scissors":
+            if (computerSelection === "Rock") {
+                return "You lose (rock beat scissors)!";
+            }
+            else if (computerSelection === "Paper") {
+                return "You win (scissors beat paper)!";
+            }
+            else {
+                return "Tie (You both chose scissors)!";
+            }
+            break;
+    }
+}
